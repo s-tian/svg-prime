f247dd8981a48bee3ef2bf2c4687c3bac4bf4c96
Stephen Tian <stephentian@stanford.edu>
2022-08-10 15:49:32 -0700

    Reconfigure module setup again

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='svg-prime',
+    name='svg_prime',
     version='0.1',
-    packages=[''],
+    packages=['svg_prime'],
     license='MIT License',
 )
--- svg_prime/train_svg_prime.py
+++ svg_prime/train_svg_prime.py
@@ -10,7 +10,7 @@ import progressbar
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--lr', default=0.002, type=float, help='learning rate')
+parser.add_argument('--lr', default=0.0003, type=float, help='learning rate')
 parser.add_argument('--beta1', default=0.9, type=float, help='momentum term for adam')
 parser.add_argument('--batch_size', default=100, type=int, help='batch size')
 parser.add_argument('--log_dir', default='logs/lp', help='base directory to save logs')
@@ -18,28 +18,36 @@ parser.add_argument('--model_dir', default='', help='base directory to save logs
 parser.add_argument('--name', default='', help='identifier for directory')
 parser.add_argument('--data_root', default='data', help='root directory for data')
 parser.add_argument('--optimizer', default='adam', help='optimizer to train with')
+parser.add_argument('--loss_fn', default='mse', help='loss function to use (mse | l1)')
 parser.add_argument('--niter', type=int, default=300, help='number of epochs to train for')
 parser.add_argument('--seed', default=1, type=int, help='manual seed')
 parser.add_argument('--epoch_size', type=int, default=600, help='epoch size')
 parser.add_argument('--image_width', type=int, default=64, help='the height / width of the input image to network')
 parser.add_argument('--channels', default=1, type=int)
 parser.add_argument('--dataset', default='smmnist', help='dataset to train with')
+parser.add_argument('--dataset_files', nargs='+', required=False, help='dataset files for perceptual_metrics dataset')
+parser.add_argument('--camera_name', type=str, default="agentview_shift_2", help='camera name for robomimic')
+parser.add_argument('--cache_mode', type=str, default="low_dim", help='perceptual_metrics dataset cache mode')
 parser.add_argument('--n_past', type=int, default=5, help='number of frames to condition on')
 parser.add_argument('--n_future', type=int, default=10, help='number of frames to predict during training')
 parser.add_argument('--n_eval', type=int, default=30, help='number of frames to predict during eval')
-parser.add_argument('--rnn_size', type=int, default=256, help='dimensionality of hidden layer')
+parser.add_argument('--rnn_size', type=int, default=512, help='dimensionality of hidden layer')
+parser.add_argument('--M', type=int, default=1, help='scaling factor for LSTMs')
+parser.add_argument('--K', type=int, default=1, help='scaling factor for encoder/decoder')
 parser.add_argument('--prior_rnn_layers', type=int, default=1, help='number of layers')
 parser.add_argument('--posterior_rnn_layers', type=int, default=1, help='number of layers')
 parser.add_argument('--predictor_rnn_layers', type=int, default=2, help='number of layers')
 parser.add_argument('--z_dim', type=int, default=10, help='dimensionality of z_t')
 parser.add_argument('--g_dim', type=int, default=128, help='dimensionality of encoder output vector and decoder input vector')
+parser.add_argument('--a_dim', type=int, default=4, help='dimensionality of action vector')
 parser.add_argument('--beta', type=float, default=0.0001, help='weighting on KL to prior')
-parser.add_argument('--model', default='dcgan', help='model type (dcgan | vgg)')
+parser.add_argument('--model', default='shallow_vgg', help='model type (dcgan | vgg | shallow_vgg)')
 parser.add_argument('--data_threads', type=int, default=5, help='number of data loading threads')
 parser.add_argument('--num_digits', type=int, default=2, help='number of digits for moving mnist')
 parser.add_argument('--last_frame_skip', action='store_true', help='if true, skip connections go between frame t and frame t+t rather than last ground truth frame')
-parser.add_argument('--wandb_project_name', type=str, default="svg-prime", help='project name to use when logging with wandb')
+parser.add_argument('--wandb_project_name', type=str, default="svg_prime", help='project name to use when logging with wandb')
 parser.add_argument('--disable_wandb', action='store_true', help='do not log to wandb')
+
 
 
 opt = parser.parse_args()
@@ -53,7 +61,7 @@ if opt.model_dir != '':
     opt.model_dir = model_dir
     opt.log_dir = '%s/continued' % opt.log_dir
 else:
-    name = 'model=%s%dx%d-rnn_size=%d-predictor-posterior-prior-rnn_layers=%d-%d-%d-n_past=%d-n_future=%d-lr=%.4f-g_dim=%d-z_dim=%d-last_frame_skip=%s-beta=%.7f%s' % (opt.model, opt.image_width, opt.image_width, opt.rnn_size, opt.predictor_rnn_layers, opt.posterior_rnn_layers, opt.prior_rnn_layers, opt.n_past, opt.n_future, opt.lr, opt.g_dim, opt.z_dim, opt.last_frame_skip, opt.beta, opt.name)
+    name = opt.name
     if opt.dataset == 'smmnist':
         opt.log_dir = '%s/%s-%d/%s' % (opt.log_dir, opt.dataset, opt.num_digits, name)
     else:
@@ -83,41 +91,32 @@ elif opt.optimizer == 'sgd':
 else:
     raise ValueError('Unknown optimizer: %s' % opt.optimizer)
 
+assert opt.model == "shallow_vgg", "SVG' uses the shallow VGG model"
+import svg_prime.models.shallow_vgg as model
 
-import models.lstm as lstm_models
+if opt.model_dir != '':
+    decoder = saved_model['decoder']
+    encoder = saved_model['encoder']
+else:
+    encoder = model.encoder(opt.g_dim, opt.channels, expand=opt.K)
+    decoder = model.decoder(opt.g_dim, opt.channels, expand=opt.K)
+    encoder.apply(utils.init_weights)
+    decoder.apply(utils.init_weights)
+
+
+import svg_prime.models.conv_lstm as lstm_models
 if opt.model_dir != '':
     frame_predictor = saved_model['frame_predictor']
     posterior = saved_model['posterior']
     prior = saved_model['prior']
 else:
-    frame_predictor = lstm_models.lstm(opt.g_dim+opt.z_dim, opt.g_dim, opt.rnn_size, opt.predictor_rnn_layers, opt.batch_size)
-    posterior = lstm_models.gaussian_lstm(opt.g_dim, opt.z_dim, opt.rnn_size, opt.posterior_rnn_layers, opt.batch_size)
-    prior = lstm_models.gaussian_lstm(opt.g_dim, opt.z_dim, opt.rnn_size, opt.prior_rnn_layers, opt.batch_size)
+    frame_predictor = lstm_models.ConvLSTM(opt.g_dim+opt.z_dim+opt.a_dim, opt.g_dim, opt.rnn_size, (8, 8), opt.predictor_rnn_layers, opt.batch_size, expand=opt.M)
+    posterior = lstm_models.ConvGaussianLSTM(opt.g_dim, opt.z_dim, opt.rnn_size, (8, 8), opt.posterior_rnn_layers, opt.batch_size, expand=opt.M)
+    prior = lstm_models.ConvGaussianLSTM(opt.g_dim, opt.z_dim, opt.rnn_size, (8, 8), opt.prior_rnn_layers, opt.batch_size, expand=opt.M)
     frame_predictor.apply(utils.init_weights)
     posterior.apply(utils.init_weights)
     prior.apply(utils.init_weights)
 
-if opt.model == 'dcgan':
-    if opt.image_width == 64:
-        import models.dcgan_64 as model 
-    elif opt.image_width == 128:
-        import models.dcgan_128 as model  
-elif opt.model == 'vgg':
-    if opt.image_width == 64:
-        import models.vgg_64 as model
-    elif opt.image_width == 128:
-        import models.vgg_128 as model
-else:
-    raise ValueError('Unknown model: %s' % opt.model)
-       
-if opt.model_dir != '':
-    decoder = saved_model['decoder']
-    encoder = saved_model['encoder']
-else:
-    encoder = model.encoder(opt.g_dim, opt.channels)
-    decoder = model.decoder(opt.g_dim, opt.channels)
-    encoder.apply(utils.init_weights)
-    decoder.apply(utils.init_weights)
 
 frame_predictor_optimizer = opt.optimizer(frame_predictor.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 posterior_optimizer = opt.optimizer(posterior.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
@@ -126,12 +125,16 @@ encoder_optimizer = opt.optimizer(encoder.parameters(), lr=opt.lr, betas=(opt.be
 decoder_optimizer = opt.optimizer(decoder.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 
 # --------- loss functions ------------------------------------
-mse_criterion = nn.MSELoss()
+if opt.loss_fn == "mse":
+    reconstruction_criterion = nn.MSELoss()
+elif opt.loss_fn == "l1":
+    reconstruction_criterion = nn.L1Loss()
+else:
+    raise NotImplementedError("Unknown loss function: %s" % opt.loss_fn)
+
+
 def kl_criterion(mu1, logvar1, mu2, logvar2):
-    # KL( N(mu_1, sigma2_1) || N(mu_2, sigma2_2)) = 
-    #   log( sqrt(
-    # 
-    sigma1 = logvar1.mul(0.5).exp() 
+    sigma1 = logvar1.mul(0.5).exp()
     sigma2 = logvar2.mul(0.5).exp() 
     kld = torch.log(sigma2/sigma1) + (torch.exp(logvar1) + (mu1 - mu2)**2)/(2*torch.exp(logvar2)) - 1/2
     return kld.sum() / opt.batch_size
@@ -142,23 +145,63 @@ posterior.cuda()
 prior.cuda()
 encoder.cuda()
 decoder.cuda()
-mse_criterion.cuda()
+reconstruction_criterion.cuda()
 
 # --------- load a dataset ------------------------------------
-train_data, test_data = utils.load_dataset(opt)
+def wrap_prep_data(fn):
+    def prep(x):
+        data_dict = fn(x)
+        video = data_dict["video"].permute(1, 0, 2, 3, 4).cuda()
+        actions = data_dict["actions"].permute(1, 0, 2).cuda()
+        return video, actions
+    return prep
 
-train_loader = DataLoader(train_data,
-                          num_workers=opt.data_threads,
-                          batch_size=opt.batch_size,
-                          shuffle=True,
-                          drop_last=True,
-                          pin_memory=True)
-test_loader = DataLoader(test_data,
-                         num_workers=opt.data_threads,
-                         batch_size=opt.batch_size,
-                         shuffle=True,
-                         drop_last=True,
-                         pin_memory=True)
+if opt.dataset == "perceptual_metrics":
+    from fitvid.data.robomimic_data import load_dataset_robomimic_torch
+    train_loader, train_prep = load_dataset_robomimic_torch(
+        dataset_path=opt.dataset_files,
+        batch_size=opt.batch_size,
+        video_len=opt.n_past + opt.n_future,
+        video_dims=(64, 64),
+        phase="train",
+        depth=False,
+        normal=False,
+        view=opt.camera_name,
+        cache_mode=opt.cache_mode,
+        seg=False,
+    )
+    test_loader, test_prep = load_dataset_robomimic_torch(
+        dataset_path=opt.dataset_files,
+        batch_size=opt.batch_size,
+        video_len=opt.n_past + opt.n_future,
+        video_dims=(64, 64),
+        phase="valid",
+        depth=False,
+        normal=False,
+        view=opt.camera_name,
+        cache_mode=opt.cache_mode,
+        seg=False,
+    )
+    train_prep, test_prep = wrap_prep_data(train_prep), wrap_prep_data(test_prep)
+
+else:
+    train_data, test_data = utils.load_dataset(opt)
+
+    train_loader = DataLoader(train_data,
+                              num_workers=opt.data_threads,
+                              batch_size=opt.batch_size,
+                              shuffle=True,
+                              drop_last=True,
+                              pin_memory=True)
+    test_loader = DataLoader(test_data,
+                             num_workers=opt.data_threads,
+                             batch_size=opt.batch_size,
+                             shuffle=True,
+                             drop_last=True,
+                             pin_memory=True)
+
+    train_prep = test_prep = lambda x: x
+
 
 def get_training_batch():
     while True:
@@ -175,7 +218,7 @@ def get_testing_batch():
 testing_batch_generator = get_testing_batch()
 
 # --------- plotting funtions ------------------------------------
-def plot(x, epoch):
+def plot(x, epoch, actions=None):
     nsample = 20 
     gen_seq = [[] for i in range(nsample)]
     gt_seq = [x[i] for i in range(len(x))]
@@ -192,24 +235,32 @@ def plot(x, epoch):
                 h, skip = h
             else:
                 h, _ = h
-            h = h
             if i < opt.n_past:
                 h_target = encoder(x[i])
                 h_target = h_target[0]
                 z_t, _, _ = posterior(h_target)
                 prior(h)
-                frame_predictor(torch.cat([h, z_t], 1))
+                if actions is not None:
+                    tiled_action = utils.tile_actions_into_image(actions[i - 1], (h.shape[-2:]))
+                    frame_predictor(torch.cat([h, tiled_action, z_t], 1))
+                else:
+                    frame_predictor(torch.cat([h, z_t], 1))
                 x_in = x[i]
                 gen_seq[s].append(x_in)
             else:
                 z_t, _, _ = prior(h)
-                h = frame_predictor(torch.cat([h, z_t], 1))
+                if actions is not None:
+                    tiled_action = utils.tile_actions_into_image(actions[i - 1], (h.shape[-2:]))
+                    h = frame_predictor(torch.cat([h, tiled_action, z_t], 1))
+                else:
+                    h = frame_predictor(torch.cat([h, z_t], 1))
                 x_in = decoder([h, skip])
                 gen_seq[s].append(x_in)
 
     to_plot = []
     gifs = [ [] for t in range(opt.n_eval) ]
     nrow = min(opt.batch_size, 10)
+    eval_mse = []
     for i in range(nrow):
         # ground truth sequence
         row = [] 
@@ -221,13 +272,15 @@ def plot(x, epoch):
         min_mse = 1e7
         for s in range(nsample):
             mse = 0
+            sample_eval_mse = 0
             for t in range(opt.n_eval):
                 mse +=  torch.sum( (gt_seq[t][i].data.cpu() - gen_seq[s][t][i].data.cpu())**2 )
+                sample_eval_mse += reconstruction_criterion(gt_seq[t][i].data.cpu(), gen_seq[s][t][i].data.cpu())
             if mse < min_mse:
                 min_mse = mse
                 min_idx = s
-
-        s_list = [min_idx, 
+        eval_mse.append(sample_eval_mse / opt.n_eval)
+        s_list = [min_idx,
                   np.random.randint(nsample), 
                   np.random.randint(nsample), 
                   np.random.randint(nsample), 
@@ -245,15 +298,31 @@ def plot(x, epoch):
                 s = s_list[ss]
                 row.append(gen_seq[s][t][i])
             gifs[t].append(row)
-
+    wandb.log({'eval/mse': np.mean(eval_mse)})
     fname = '%s/gen/sample_%d.png' % (opt.log_dir, epoch) 
     utils.save_tensors_image(fname, to_plot)
 
     fname = '%s/gen/sample_%d.gif' % (opt.log_dir, epoch) 
     utils.save_gif(fname, gifs)
+    log_wandb_gif(gifs, prefix="eval")
 
 
-def plot_rec(x, epoch):
+def recursive_stack(list_of_tensors):
+    if torch.is_tensor(list_of_tensors):
+        return list_of_tensors
+    else:
+        return torch.stack([recursive_stack(t) for t in list_of_tensors], 0)
+
+def log_wandb_gif(gif, prefix):
+    # Convert list of list of list of tensors into one tensor
+    gif = recursive_stack(gif).detach().cpu()
+    # Take ground truth and a single random sample to log
+    gif = torch.cat((gif[:, :, 0], gif[:, :, -1]), dim=-1)
+    gif = torch.cat(torch.unbind(gif, dim=1), dim=-2)
+    gif = (gif.numpy() * 255).astype(np.uint8)
+    wandb.log({f"{prefix}/video": wandb.Video(gif)})
+
+def plot_rec(x, epoch, actions=None):
     frame_predictor.hidden = frame_predictor.init_hidden()
     posterior.hidden = posterior.init_hidden()
     gen_seq = []
@@ -271,10 +340,18 @@ def plot_rec(x, epoch):
         h_target = h_target
         z_t, _, _= posterior(h_target)
         if i < opt.n_past:
-            frame_predictor(torch.cat([h, z_t], 1)) 
+            if actions is not None:
+                tiled_action = utils.tile_actions_into_image(actions[i - 1], (h.shape[-2:]))
+                frame_predictor(torch.cat([h, tiled_action, z_t], 1))
+            else:
+                frame_predictor(torch.cat([h, z_t], 1))
             gen_seq.append(x[i])
         else:
-            h_pred = frame_predictor(torch.cat([h, z_t], 1))
+            if actions is not None:
+                tiled_action = utils.tile_actions_into_image(actions[i - 1], (h.shape[-2:]))
+                h_pred = frame_predictor(torch.cat([h, tiled_action, z_t], 1))
+            else:
+                h_pred = frame_predictor(torch.cat([h, z_t], 1))
             x_pred = decoder([h_pred, skip])
             gen_seq.append(x_pred)
    
@@ -290,7 +367,7 @@ def plot_rec(x, epoch):
 
 
 # --------- training funtions ------------------------------------
-def train(x):
+def train(x, actions=None, log_gif=False):
     frame_predictor.zero_grad()
     posterior.zero_grad()
     prior.zero_grad()
@@ -304,31 +381,46 @@ def train(x):
 
     mse = 0
     kld = 0
+
+    if log_gif:
+        final_gif = []
+
     for i in range(1, opt.n_past+opt.n_future):
         h = encoder(x[i-1])
         h_target = encoder(x[i])[0]
-        if opt.last_frame_skip or i < opt.n_past:	
+        if opt.last_frame_skip or i < opt.n_past:
             h, skip = h
         else:
             h = h[0]
         z_t, mu, logvar = posterior(h_target)
         _, mu_p, logvar_p = prior(h)
-        h_pred = frame_predictor(torch.cat([h, z_t], 1))
+        if actions is not None:
+            tiled_action = utils.tile_actions_into_image(actions[i - 1], (h.shape[-2:]))
+            h_pred = frame_predictor(torch.cat([h, tiled_action, z_t], 1))
+        else:
+            h_pred = frame_predictor(torch.cat([h, z_t], 1))
         x_pred = decoder([h_pred, skip])
-        mse += mse_criterion(x_pred, x[i])
+        if log_gif:
+            final_gif.append(x_pred)
+        mse += reconstruction_criterion(x_pred, x[i])
         kld += kl_criterion(mu, logvar, mu_p, logvar_p)
 
     loss = mse + kld*opt.beta
     loss.backward()
-
     frame_predictor_optimizer.step()
     posterior_optimizer.step()
     prior_optimizer.step()
     encoder_optimizer.step()
     decoder_optimizer.step()
 
+    if log_gif:
+        final_gif = torch.stack(final_gif, dim=0).detach().cpu()
+        gif_to_log = torch.stack((x[1:].detach().cpu(), final_gif), axis=2)
+        gif_to_log = gif_to_log[:, :10] # Log at most 10 different batch items
+        log_wandb_gif(gif_to_log, prefix="train")
 
     return mse.data.cpu().numpy()/(opt.n_past+opt.n_future), kld.data.cpu().numpy()/(opt.n_future+opt.n_past)
+
 
 # Initialize wandb
 if not opt.disable_wandb:
@@ -339,6 +431,9 @@ if not opt.disable_wandb:
         mode="online",
         settings=wandb.Settings(start_method="fork"),
     )
+    wandb.run.name = opt.name
+    wandb.run.save()
+    wandb.config.update(opt)
 
 # --------- training loop ------------------------------------
 for epoch in range(opt.niter):
@@ -352,13 +447,15 @@ for epoch in range(opt.niter):
     progress = progressbar.ProgressBar(max_value=opt.epoch_size).start()
     for i in range(opt.epoch_size):
         progress.update(i+1)
-        x = next(training_batch_generator)
-
-        # train frame_predictor 
-        mse, kld = train(x)
+        if opt.a_dim == 0:
+            x = train_prep(next(training_batch_generator))
+            actions = None
+        else:
+            x, actions = train_prep(next(training_batch_generator))
+        # train frame_predictor
+        mse, kld = train(x, actions, log_gif=(i == opt.epoch_size-1))
         epoch_mse += mse
         epoch_kld += kld
-
         if not opt.disable_wandb:
             wandb.log({
                 "train/mse": mse,
@@ -376,11 +473,16 @@ for epoch in range(opt.niter):
     decoder.eval()
     posterior.eval()
     prior.eval()
-    
-    x = next(testing_batch_generator)
+
+    if opt.a_dim == 0:
+        x = test_prep(next(testing_batch_generator))
+        actions = None
+    else:
+        x, actions = test_prep(next(testing_batch_generator))
+
     with torch.no_grad():
-        plot(x, epoch)
-        plot_rec(x, epoch)
+        plot(x, epoch, actions=actions)
+        plot_rec(x, epoch, actions=actions)
 
     # save the model
     torch.save({
